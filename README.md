d1afaed1e4951ff43f4f9880fbf402681711756a
Aya-Mohamed-Ashraf <aya.mohamed.ashraf@gmail.com>
2019-05-14 12:56:22 +0200

    Enhancing Map Selector tab

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Carologic SDK
 
+![GitHub License](https://img.shields.io/github/license/cartologic/sdk.svg)
 ![Contributers](https://img.shields.io/github/contributors/cartologic/sdk.svg)
-[![NPM version](https://img.shields.io/npm/v/sdk.svg)](https://www.npmjs.com/package/sdk)
+![package.json version](https://img.shields.io/github/package-json/v/cartologic/sdk.svg)
+![Travis (.org) branch](https://img.shields.io/travis/cartologic/sdk/master.svg)
 
-
-
-Cartoview SDK is a NPM Package Which Contains services and helpers for [Cartoview](https://github.com/cartologic/cartoview_2) Apps Development
-
+Cartoview SDK is a NPM Package Which Contains services and helpers for [Cartoview](https://github.com/cartologic/cartoview/tree/cartoview2) Apps Development
--- src/wizard/api/index.js
+++ src/wizard/api/index.js
@@ -20,28 +20,38 @@ function getCurrentUser() {
     return apiInstance.get(`users/current_user/`);
 }
 
-export function getMaps(offset, limit, showOnlyUserMaps) {
+export function getMaps(offset, limit, showOnlyUserMaps, filter = null) {
+    let filterPart = '';
+    if (filter != null) {
+        filterPart = '&' + filter.name + '=' + filter.value;
+    }
     if (showOnlyUserMaps) {
         return getCurrentUser().then(
             response => {
-                return apiInstance.get(`maps/?offset=${offset}&limit=${limit}&owner=${response.data.username}`);
+                return apiInstance.get(`maps/?offset=${offset}&limit=${limit}&owner=${response.data.username}` + filterPart);
             });
-    } else
-        return apiInstance.get(`maps/?offset=${offset}&limit=${limit}`);
+    } else {
+        return apiInstance.get(`maps/?offset=${offset}&limit=${limit}` + filterPart);
+    }
 }
 
 export function getMap(mapId) {
     return apiInstance.get('maps/' + mapId);
 };
 
-export function getMapsByTitle(offset, limit, title, showOnlyUserMaps) {
+export function getMapsByTitle(offset, limit, title, showOnlyUserMaps, filter) {
+    let filterPart = '';
+    if (filter != null) {
+        filterPart = '&' + filter.name + '=' + filter.value;
+    }
     if (showOnlyUserMaps) {
         return getCurrentUser().then(
             response => {
-                return apiInstance.get(`maps/?offset=${offset}&limit=${limit}&title__icontains=${title}&owner=${response.data.username}`);
+                return apiInstance.get(`maps/?offset=${offset}&limit=${limit}&title__icontains=${title}&owner=${response.data.username}` + filterPart);
             });
-    } else
-        return apiInstance.get(`maps/?offset=${offset}&limit=${limit}&title__icontains=${title}`);
+    } else {
+        return apiInstance.get(`maps/?offset=${offset}&limit=${limit}&title__icontains=${title}` + filterPart);
+    }
 }
 
 export function getUsers() {
